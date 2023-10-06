1bbecb72b1fb6b2b0610c4d75af238dab5d5e98f
MeenaAlagiah <117711147+MeenaAlagiah@users.noreply.github.com>
2023-10-06 16:41:50 +0530

    Committing the Vue Radio Button and Checkbox Sample

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RadioButtonComponent as EjsRadiobutton } from '@syncfusion/ej2-vue-buttons';
+import { CheckBoxComponent as EjsCheckbox } from '@syncfusion/ej2-vue-buttons';
+</script>
+
+<template>
+  <ejs-checkbox label="Default" checked="true"
+                cssClass="e-small" enableRtl="true" disabled="true"></ejs-checkbox>
+
+    
+<!-- <ul>
+  <li><ejs-radiobutton label="Credit/Debit Card" name="payment" checked="true"
+                       cssClass="e-small" enableRtl="true"></ejs-radiobutton></li>
+  <li><ejs-radiobutton label="Net Banking" name="payment"
+                       cssClass="e-small" enableRtl="true"></ejs-radiobutton></li>
+  <li><ejs-radiobutton label="Cash on Delivery" name="payment"
+                       cssClass="e-small" enableRtl="true" disabled="true"></ejs-radiobutton></li>
+  <li><ejs-radiobutton label="Others" name="payment"
+                       cssClass="e-small" enableRtl="true"></ejs-radiobutton></li>
+</ul> -->
+</template>
+
+<style>
+ @import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+ @import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+.e-radio-wrapper {
+  margin-top: 18px;
+}
+.e-checkbox-wrapper {
+  margin-top: 18px;
+}
+li {
+  list-style: none;
+}
+</style>
